a1c6edc61b9549bf93bad3f5c79b64066522a113
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-10-25 23:37:09 +0100

    feat(crud-pensamento): crud done

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -17,6 +17,10 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento)
   }
 
+  editar(pensamento: Pensamento): Observable<Pensamento>{
+    return this.http.put<Pensamento>(`${this.API}/${pensamento.id}`, pensamento)
+  }
+
   excluir(id: number): Observable<Pensamento>{
     return this.http.delete<Pensamento>(`${this.API}/${id}`)
   }
